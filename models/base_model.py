4f1156abf54a112c9c6759172c35cf6c4aa4d328
BarbaraStanley <barbeaboo@gmail.com>
2022-11-02 01:42:02 +0000

    update

--- models/base_model.py
+++ models/base_model.py
@@ -3,18 +3,17 @@
 
 from datetime import datetime
 from uuid import uuid4
+from models import storage
 
 
 class BaseModel:
     ''' Base/ parent class'''
-    id = Column(String(60), primary_key=True, nullable=False)
-    created_at = Column(DateTime, nullable=False, default=datetime.utcnow())
-    updated_at = Column(DateTime, nullable=False, default=datetime.utcnow())
 
-    def __init__():
+    def __init__(self, *args, **kwargs):
         '''Class initialization'''
         self.id = str(uuid4())
-        self.created_at = self.updated_at = datetime.utcnow()
+        self.created_at = self.updated_at = datetime.now()
+        storage.new(self)
         if kwargs:
             for k, v in kwargs.items():
                 if k == "created_at" or k == "updated_at":
@@ -24,9 +23,8 @@ class BaseModel:
 
     def save(self):
         """Update updated_at with the current datetime."""
-        self.updated_at = datetime.utcnow()
-        models.storage.new(self)
-        models.storage.save()
+        self.updated_at = datetime.now()
+        storage.save()
 
     def to_dict(self, remove_password=True):
         """Return a dictionary representation of the BaseModel instance.
@@ -37,18 +35,10 @@ class BaseModel:
         dictionary["__class__"] = str(type(self).__name__)
         dictionary["created_at"] = self.created_at.isoformat()
         dictionary["updated_at"] = self.updated_at.isoformat()
-        dictionary.pop("_sa_instance_state", None)
-        if remove_password is True:
-            dictionary.pop("password", None)
         return dictionary
 
-    def delete(self):
-        """Delete the current instance from storage."""
-        models.storage.delete(self)
-
     def __str__(self):
-        """Return the print/str representation of the BaseModel instance."""
+        """Return the string representation of the BaseModel instance."""
         d = self.__dict__.copy()
-        d.pop("_sa_instance_state", None)
         return "[{}] ({}) {}".format(type(self).__name__, self.id, d)
 
