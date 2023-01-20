3113cc0fe53c6c1553fb4b89328999c040014bdf
Barbara <barbeaboo@gmail.com>
2023-01-20 15:16:17 +0000

    update to init method

--- models/base_model.py
+++ models/base_model.py
@@ -7,11 +7,18 @@ from uuid import uuid4
 
 class BaseModel:
     ''' Base/parent class '''
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         ''' constructor to initialize class '''
         self.id = str(uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
+
+        if kwargs:
+            for k, v in kwargs.items():
+                if k == "created_at" or k == "updated_at":
+                    v = datetime.strptime(v, "%Y-%m-%dT%H:%M:%S.%f")
+                if k != "__class__":
+                    setattr(self, k, v)
 
     def __str__(self):
         d = self.__dict__.copy()
