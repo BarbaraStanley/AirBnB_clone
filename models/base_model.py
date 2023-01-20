c5e967e3cd6c9bcc4f79f1ddb91c3908d45fe70c
Barbara <barbeaboo@gmail.com>
2023-01-20 14:27:12 +0000

    first base model correction, starting from scratch

--- models/base_model.py
+++ models/base_model.py
@@ -3,42 +3,29 @@
 
 from datetime import datetime
 from uuid import uuid4
-from models import storage
 
 
 class BaseModel:
-    ''' Base/ parent class'''
-
-    def __init__(self, *args, **kwargs):
-        '''Class initialization'''
+    ''' Base/parent class '''
+    def __init__(self):
+        ''' constructor to initialize class '''
         self.id = str(uuid4())
-        self.created_at = self.updated_at = datetime.now()
-        storage.new(self)
-        if kwargs:
-            for k, v in kwargs.items():
-                if k == "created_at" or k == "updated_at":
-                    v = datetime.strptime(v, "%Y-%m-%dT%H:%M:%S.%f")
-                if k != "__class__":
-                    setattr(self, k, v)
-
-    def save(self):
-        """Update updated_at with the current datetime."""
+        self.created_at = datetime.now()
         self.updated_at = datetime.now()
-        storage.save()
-
-    def to_dict(self, remove_password=True):
-        """Return a dictionary representation of the BaseModel instance.
-        Includes the key/value pair __class__ representing the class name
-        of the object
-        """
-        dictionary = self.__dict__.copy()
-        dictionary["__class__"] = str(type(self).__name__)
-        dictionary["created_at"] = self.created_at.isoformat()
-        dictionary["updated_at"] = self.updated_at.isoformat()
-        return dictionary
 
     def __str__(self):
-        """Return the string representation of the BaseModel instance."""
         d = self.__dict__.copy()
-        return "[{}] ({}) {}".format(type(self).__name__, self.id, d)
+        return "[{}] ({}) <{}>".format(type(self).__name__, self.id, d)
 
+    def save(self):
+        ''' updates the updated_at with the current datetime'''
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        ''' dictionary containing all keys/values of __dict__ of instance '''
+
+        dic = self.__dict__.copy()
+        dic["__class__"] = str(type(self).__name__)
+        dic["created_at"] = self.created_at.isoformat()
+        dic["updated_at"] = self.updated_at.isoformat()
+        return dic
