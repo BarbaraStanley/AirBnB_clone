db033819dc17a63f2501a813e910eecb8d0b0346
Barbara <barbeaboo@gmail.com>
2023-02-15 14:24:27 +0000

    Basem

--- models/base_model.py
+++ models/base_model.py
@@ -29,7 +29,7 @@ class BaseModel:
     def save(self):
         ''' updates the updated_at with the current datetime'''
         self.updated_at = datetime.now()
-        models.storage.save()
+        #models.storage.save()
 
     def to_dict(self):
         ''' dictionary containing all keys/values of __dict__ of instance '''
